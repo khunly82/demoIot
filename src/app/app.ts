8f4d0be51a8f665eab1a405ac6aa3a01698fbd71
Khun <lykhun@gmail.com>
2025-08-11 16:43:41 +0200

    ex1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,19 @@
 
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from "@angular/router";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
 
+  open = true;
+
+  clickHandler() {
+    this.open = !this.open;
+  }
 }
