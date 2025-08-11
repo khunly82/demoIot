b9e979a241cb85e49aba4e219463c3a3e794dddb
Khun <lykhun@gmail.com>
2025-08-11 13:51:40 +0200

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // si on ne met rien, le router redirige vers home
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'login', component: Login },
+];
