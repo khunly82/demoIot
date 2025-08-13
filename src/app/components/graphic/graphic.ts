a3b78308fdc2c92cdd9e0b2ede43a6bf4e161abd
phil-bstorm <philippe.haerens@bstorm.be>
2025-08-13 16:36:30 +0200

    [Add] Chart.js

--- src/app/components/graphic/graphic.ts
+++ src/app/components/graphic/graphic.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RealtimeStatus } from '../../shared/components/realtime-status/realtime-status';
+import { SensorsService } from '../../services/sensors-service';
+import { ChartData, ChartOptions, ScatterDataPoint } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import 'chartjs-adapter-date-fns';
+
+type XY = ScatterDataPoint; // {x: number, y: number}
 
 @Component({
   selector: 'app-graphic',
-  imports: [RealtimeStatus],
+  imports: [RealtimeStatus, BaseChartDirective],
   templateUrl: './graphic.html',
   styleUrl: './graphic.css',
 })
-export class Graphic {}
+export class Graphic {
+  private sensorsService = inject(SensorsService);
+
+  chartType: 'line' = 'line';
+
+  // Data
+  chartData = computed<ChartData<'line', XY[], number>>(() => {
+    return {
+      datasets: [
+        {
+          label: 'Temperature',
+          data: (this.sensorsService.data() ?? []).map((d) => ({
+            x: new Date(d.date).getTime(),
+            y: d.temperature,
+          })),
+
+          // affichages
+          borderColor: 'red',
+          pointRadius: 5,
+        },
+
+        {
+          label: 'Humidité',
+          data: (this.sensorsService.data() ?? []).map((d) => ({
+            x: new Date(d.date).getTime(),
+            y: d.humidity,
+          })),
+
+          // affichages
+          borderColor: 'blue',
+          pointRadius: 3,
+        },
+      ],
+    };
+  });
+
+  chartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    scales: {
+      x: {
+        type: 'time',
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+}
