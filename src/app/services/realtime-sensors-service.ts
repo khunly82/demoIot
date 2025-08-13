995feba7cc67dd49ed3ca2f5835c1d2c75dc147e
phil-bstorm <philippe.haerens@bstorm.be>
2025-08-13 11:47:05 +0200

    [Add] SignalR connection

---
+++ src/app/services/realtime-sensors-service.ts
@@ -0,0 +1,31 @@
+import { Injectable, signal } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RealtimeSensorsService {
+  private connection: HubConnection | null = null;
+
+  // Etat de la connexion
+  isConnected = signal(false);
+
+  constructor() {
+    this.startConnectionAsync();
+  }
+
+  async startConnectionAsync() {
+    // préparation de la connexion
+    this.connection = new HubConnectionBuilder()
+      .withUrl('http://localhost:5097/realtime-sensors')
+      .build();
+
+    // connexion au serveur
+    this.connection.start();
+
+    if (this.connection.connectionId) {
+      // Nous somme connecté!
+      console.log('SignalR connectionId: ' + this.connection.connectionId);
+    }
+  }
+}
--- src/app/services/sensors-service.ts
+++ src/app/services/sensors-service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { Data } from '../../@types/data';
 import { HttpClient } from '@angular/common/http';
+import { RealtimeSensorsService } from './realtime-sensors-service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,7 @@ export class SensorsService {
   data = signal<Data[] | null>(null);
 
   private httpClient = inject(HttpClient);
+  private realtimeSensor = inject(RealtimeSensorsService);
 
   constructor() {
     this.fetchSensorsData();
